6b9091926bab93f0dfc2b4a92fc51355238b5f7c
lucas <lucas@telium.com.br>
2024-06-24 01:14:41 -0300

    fix: repository

--- aaa/src/main/factories/use-cases/aaaa.ts
+++
@@ -1,7 +0,0 @@
-import { RemoteAaaa } from "@/data";
-import { Aaaa } from "@/domain/use-cases";
-import { makeApiUrl } from "@/main/factories/http";
-import { makeAuthorizeHttpClientDecorator } from "@/main/factories/decorators";
-
-export const makeRemoteAaaa = (): Aaaa =>
-  new RemoteAaaa(makeApiUrl("/contact/contacts/{id}"), makeAuthorizeHttpClientDecorator());
--- src/main/adapter/index.ts
+++ src/main/adapter/index.ts
@@ -5,12 +5,22 @@ interface Options {
 }
 
 export default (name: string, options: Options, fullpath: string): void => {
-  const { test, properties, onlyTest, ...rest } = options
+  const { test, properties, onlyTest,multipleMethods, ...rest } = options
   const allroutes = routes()
+  console.log(multipleMethods);
+
   const keys = Object.keys(rest)
   keys.forEach(element => {
     try {
-     allroutes[element]?.handle(fullpath, name, test, properties, onlyTest);
+      if(allroutes[element]){
+        if(multipleMethods){
+          (multipleMethods as any as string).split(',').map(item => {
+            allroutes[element]?.handle(fullpath, `${name}/${item}`, test, properties, onlyTest);
+          })
+          return
+        }
+        allroutes[element]?.handle(fullpath, name, test, properties, onlyTest);
+      }
     } catch (error) {
       console.log(error)
     }
